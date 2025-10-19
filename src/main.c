7b72ff857f2ee8deb99b649d1938591523852c58
Saeed Ahmed <saeedmedra@gmail.com>
2025-10-19 20:52:53 +0300

    Create a TCP server socket

--- src/main.c
+++ src/main.c
@@ -3,12 +3,23 @@
 
 int main() {
     WSADATA wsa;
+    SOCKET server_socket;
 
     // init winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         printf("Failed to initialize Winsock. Error Code: %d\n", WSAGetLastError());
         return 1;
     }
+
+    //create TCP socket
+    server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server_socket == INVALID_SOCKET) {
+        printf("Socket creation failed. Error Code: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Socket created successfully\n");
 
     printf("Winsock initialized successfully\n");
 
